1dfd5ff938de06c77a62659405069cdf7ed73e7f
Daniel Sobral <dcsobral@gmail.com>
2021-01-12 12:05:32 -0700

    Version 1.0

---
+++ FoundationCount/FoundationCount.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FoundationCount : Mod
+{
+    public void Start()
+    {
+        Debug.Log("Mod FoundationCount has been loaded!");
+    }
+
+    [ConsoleCommand(name: "foundations", docs: "Shows the number of foundations.")]
+    public static string MyCommand(string[] args)
+    {
+        var raftBounds = UnityEngine.Object.FindObjectOfType<RaftBounds>();
+        return $"{raftBounds.FoundationCount} foundations.";
+    }
+
+    public void OnModUnload()
+    {
+        Debug.Log("Mod FoundationCount has been unloaded!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+FoundationCount Raft Mod
+========================
+
+Adds a `foundations` command to the Raft Mod Loader console that
+displays the number of foundations on the Raft.
+
